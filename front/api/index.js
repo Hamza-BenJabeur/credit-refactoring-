078e0bed4b9563121bb4133ead5162589b8ce768
Hamza-BenJabeur <hamza.mhbj@gmail.com>
2021-06-17 16:35:39 +0100

    preparing front end for fetching

---
+++ front/api/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const url = 'http://localhost:3000/credits';
+
+export const fetchCredits = () => axios.get(url);
+export const createCredit = (newCredit) => axios.post(url, newCredit);
+export const updateCreditByAdding = (id, updatedCredit) => axios.patch(`${url}/${id}/add`, updatedCredit);
+export const updateCreditBySubstracting = (id, updatedCredit) => axios.patch(`${url}/${id}/sub`, updatedCredit);
