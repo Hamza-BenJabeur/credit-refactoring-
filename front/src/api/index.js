7d509790d33ff920552004dd09b3c2fd1027848a
Hamza-BenJabeur <hamza.mhbj@gmail.com>
2021-06-17 17:18:16 +0100

    separating differents fetch

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -6,3 +6,4 @@ export const fetchCredits = () => axios.get(url);
 export const createCredit = (newCredit) => axios.post(url, newCredit);
 export const updateCreditByAdding = (id, updatedCredit) => axios.patch(`${url}/${id}/add`, updatedCredit);
 export const updateCreditBySubstracting = (id, updatedCredit) => axios.patch(`${url}/${id}/sub`, updatedCredit);
+export const deleteCredit=(id)=>axios.delete(`${url}/${id}`)
